b5b3ed25c4417a895617f82cedb6025b38e02243
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-08 20:13:38 +0700

    test 4 methods

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'));
 
+app.post('/', (req, res) => res.send('POST METHOD'));
+
+app.put('/', (req, res) => res.send('PUT METHOD'));
+
+app.delete('/', (req, res) => res.send('DELETE METHOD'));
+
 app.listen(process.env.PORT || 3000, () => console.log('Server started!'));
 
 app.locals.isDev = process.env.NODE_ENV !== 'production';
