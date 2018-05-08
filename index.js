785fb69a64f7cad238505438958076c7895dd3b5
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-08 20:29:12 +0700

    add.ejs

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-
+const { urlencoded } = require('body-parser');
 const app = express();
 
+app.use(urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'));
 
-app.post('/', (req, res) => res.send('POST METHOD'));
+app.get('/add', (req, res) => res.render('add'));
 
-app.put('/', (req, res) => res.send('PUT METHOD'));
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('POST METHOD');
+});
 
-app.delete('/', (req, res) => res.send('DELETE METHOD'));
+// app.put('/', (req, res) => res.send('PUT METHOD'));
+// app.delete('/', (req, res) => res.send('DELETE METHOD'));
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started!'));
 
