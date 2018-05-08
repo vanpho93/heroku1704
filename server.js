c2a12897b2119f1c729fd0234fe67ec8734b928b
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-08 21:07:11 +0700

    add new singer

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { urlencoded } = require('body-parser');
-const { singers } = require('./Singer');
+const { singers, Singer } = require('./Singer');
 
 const app = express();
 
@@ -12,6 +12,14 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('singer', { singers }));
 
 app.get('/add', (req, res) => res.render('create'));
+
+app.post('/add', (req, res) => {
+    const { name, link, image } = req.body;
+    const singer = new Singer(name, link, image);
+    singers.push(singer);
+    // res.send('Da nhan');
+    res.redirect('/');
+});
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started!'));
 
